74c20e9372c09b1e85e387e5abc06c894b6dac1b
SongJwans <yhh5916@naver.com>
2024-11-26 10:43:37 +0900

    init commit

---
+++ src/main/java/com/example/classicjeans/ClassicJeansApplication.java
@@ -0,0 +1,13 @@
+package com.example.classicjeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicJeansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassicJeansApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/classicjeans/ClassicJeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classicjeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicJeansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
