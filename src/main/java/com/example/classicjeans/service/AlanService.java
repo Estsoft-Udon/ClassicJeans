af29b5aca26066acba9f5bfaa6c57da910f57a4c
Sang Won Lee <freerusya@naver.com>
2024-11-27 19:18:47 +0900

    feat: 치매검사 Service 기능 추가

--- src/main/java/com/example/classicjeans/service/AlanService.java
+++ src/main/java/com/example/classicjeans/service/AlanService.java
@@ -1,8 +1,11 @@
 package com.example.classicjeans.service;
 
+import com.example.classicjeans.dto.request.AlanDementiaRequest;
 import com.example.classicjeans.dto.request.AlanHealthRequest;
 import com.example.classicjeans.dto.response.AlanBasicResponse;
+import com.example.classicjeans.dto.response.AlanDementiaResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -10,6 +13,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class AlanService {
@@ -51,5 +59,19 @@ public class AlanService {
         System.out.println(response.getBody());
 
         return objectMapper.readValue(response.getBody(), AlanBasicResponse.class);
+    }
+
+    // 치매 문진표 AI 검사
+    public AlanDementiaResponse fetchDementiaResponse(AlanDementiaRequest request) throws JsonProcessingException {
+        String CLIENT_ID = "CLIENT_ID 키 넣어야 함";
+        String uri = UriComponentsBuilder
+                .fromHttpUrl(BASE_URL)
+                .queryParam("content", request.toString())
+                .queryParam("client_id", CLIENT_ID)
+                .toUriString();
+        ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
+        System.out.println(response.getBody());
+
+        return objectMapper.readValue(response.getBody(), AlanDementiaResponse.class);
     }
 }
