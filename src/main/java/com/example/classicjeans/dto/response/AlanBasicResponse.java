36abe429911d2e5df07a15717ad422d93487ae42
SongJwans <yhh5916@naver.com>
2024-11-27 15:20:12 +0900

    feature : alan 질문 확인 기능 구현

---
+++ src/main/java/com/example/classicjeans/dto/response/AlanBasicResponse.java
@@ -0,0 +1,28 @@
+package com.example.classicjeans.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlanBasicResponse {
+    @JsonProperty(value = "action")
+    private Action action;
+
+    @JsonProperty(value = "content")
+    private String content;
+
+    @Getter
+    static class Action {
+
+        @JsonProperty(value = "name")
+        private String name;
+
+        @JsonProperty(value = "speak")
+        private String speak;
+    }
+}
