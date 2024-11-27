fe713268678a4f3dd0809f5087a31c73a2dadd7a
jingeon <ephigh1397@naver.com>
2024-11-27 11:46:14 +0900

    feat: Create Enums
    Gedner (MALE / FEMALE)
    Grade (CHUNGBAZI / ADMIN)
    
    #23

---
+++ src/main/java/com/example/classicjeans/enums/Gender.java
@@ -0,0 +1,12 @@
+package com.example.classicjeans.enums;
+
+public enum Gender {
+    MALE("남성"),
+    FEMALE("여성");
+
+    private final String displayName;
+
+    Gender(String displayName) {this.displayName = displayName;}
+
+    public String getDisplayName() {return displayName;}
+}
