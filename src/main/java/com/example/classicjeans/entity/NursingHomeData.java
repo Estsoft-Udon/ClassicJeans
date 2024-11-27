3d30cd64d6902b722f391eb93f5d65bbd6328f7b
jingeon <ephigh1397@naver.com>
2024-11-27 11:40:47 +0900

    feat: Create Entities
     Users
     Bazi (사주운세)
     HealthData (검진 정보)
     HospitalData (병원 정보)
     NursingHomeData (요양원 정보)
     SubsidyData (지원금 정보)
     ParentsInfo (부모님 정보)
    
    병원 정보에 region 어떻게 처리할 것인지 고민해봐야 합니다.
    또한 검진 정보에 어떠한 항목이 들어가는 게 좋을지 고민해봐야 합니다.
    #21

---
+++ src/main/java/com/example/classicjeans/entity/NursingHomeData.java
@@ -0,0 +1,31 @@
+package com.example.classicjeans.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class NursingHomeData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column()
+    private Double latitude;
+
+    @Column()
+    private Double longitude;
+}
