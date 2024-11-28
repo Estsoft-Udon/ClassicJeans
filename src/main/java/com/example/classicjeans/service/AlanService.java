90e44ac40c43e136cfc68f3650e621f0fafffadc
Sang Won Lee <freerusya@naver.com>
2024-11-28 19:30:58 +0900

    feat: client_id로 요청했던 이전 데이터 결과 초기화 기능

--- src/main/java/com/example/classicjeans/service/AlanService.java
+++ src/main/java/com/example/classicjeans/service/AlanService.java
@@ -9,8 +9,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -21,6 +22,7 @@ import java.util.regex.Pattern;
 public class AlanService {
 
     private static final String BASE_URL = "https://kdt-api-function.azurewebsites.net/api/v1/question";
+    private static final String DELETE_URL = "https://kdt-api-function.azurewebsites.net/api/v1/reset-state";
     private static final String CLIENT_ID = "CLIENT_ID 키 넣어야 함";
 
     private static final String SUMMARY_EVALUATION_PATTERN = "### 종합 평가 \\(Summary Evaluation\\)[\\s\\S]*?\\n-.*?\\n(.*?)\\n### 개선 방법";
@@ -64,6 +66,26 @@ public class AlanService {
 
         ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
         return response.getBody();
+    }
+
+    // 메소드 실행 전에 이전 데이터를 초기화
+    private void resetPreviousData() {
+        String uri = UriComponentsBuilder
+                .fromHttpUrl(DELETE_URL)
+                .toUriString();
+
+        String jsonBody = "{\"client_id\":\"" + CLIENT_ID + "\"}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>(jsonBody, headers);
+
+        try {
+            restTemplate.exchange(uri, HttpMethod.DELETE, entity, String.class);
+        } catch (HttpStatusCodeException e) {
+            System.err.println("Error during reset: " + e.getResponseBodyAsString());
+        }
     }
 
     // AI 응답을 AlanDementiaResponse로 변환
