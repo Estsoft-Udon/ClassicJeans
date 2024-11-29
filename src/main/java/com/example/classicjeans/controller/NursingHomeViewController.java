5dd0e91dd60d8f435e59f960b16d99ddf366ec09
dejeong <ahjeong921@gmail.com>
2024-11-29 16:28:15 +0900

    feat : 복지시설 찾기 추가

--- src/main/java/com/example/classicjeans/controller/NursingHomeViewController.java
+++ src/main/java/com/example/classicjeans/controller/NursingHomeViewController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SanatoriumViewController {
+public class NursingHomeViewController {
 
-    @GetMapping("/sanatorium_list")
-    public String sanatoriumList() {
-        return "/info/sanatorium_list";
+    @GetMapping("/nursing_list")
+    public String nursingList() {
+        return "/info/nursing_list";
     }
+
 }
