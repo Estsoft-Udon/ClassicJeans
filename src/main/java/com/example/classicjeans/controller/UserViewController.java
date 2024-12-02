2656cdeb5c112d28ecb32dab893415a421d8ac06
dejeong <ahjeong921@gmail.com>
2024-12-02 17:03:35 +0900

    feat : 마이페이지 수정

--- src/main/java/com/example/classicjeans/controller/UserViewController.java
+++ src/main/java/com/example/classicjeans/controller/UserViewController.java
@@ -5,9 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserViewController {
-    // 마이페이지
+    // 회원 정보
     @GetMapping("/mypage")
     public String myPage() {
         return "/member/mypage";
+    }
+
+    // 회원 정보 수정
+    @GetMapping("/edit_profile")
+    public String editProfile() {
+        return "/member/edit_profile";
     }
 }
