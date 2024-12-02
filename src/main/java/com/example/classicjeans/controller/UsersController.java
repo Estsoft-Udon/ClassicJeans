bc559122e964c7b73c2550e72cf048baf21ab6b7
jingeon <ephigh1397@naver.com>
2024-12-02 15:29:30 +0900

    feature: UsersController 기본 회원동작 추가
    ID찾기
    회원가입
    아이디, 닉네임, 이메일 중복체크

--- src/main/java/com/example/classicjeans/controller/UsersController.java
+++ src/main/java/com/example/classicjeans/controller/UsersController.java
@@ -2,12 +2,14 @@ package com.example.classicjeans.controller;
 
 import com.example.classicjeans.dto.request.UsersRequest;
 import com.example.classicjeans.dto.response.UsersResponse;
+import com.example.classicjeans.entity.Users;
 import com.example.classicjeans.service.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -45,4 +47,55 @@ public class UsersController {
         usersService.delete(userId);
         return ResponseEntity.ok().build();
     }
+
+
+    @PostMapping("/searchId")
+    public ResponseEntity<String> findUserBySearchId(@RequestBody UsersRequest user) {
+        Users foundUser = usersService.searchId(user.getName(), user.getEmail());
+        return ResponseEntity.ok(foundUser.getLoginId());
+    }
+
+//    @PostMapping("/searchPassword")
+//    public ResponseEntity<String> findUserByPassword(@RequestBody UsersRequest user) {
+//        Users foundUser = usersService.searchPassword(user.getLoginId(), user.getPasswordHint(),
+//                user.getPasswordAnswer());
+//        return ResponseEntity.ok(foundUser.getPassword());
+//    }
+
+    // 회원가입시 아이디 중복체크
+    @PostMapping("/checkId")
+    public ResponseEntity<Boolean> checkId(@RequestBody Map<String, String> requestBody) {
+        String loginId = requestBody.get("loginId");
+        boolean isDuplicate = usersService.isLoginIdDuplicate(loginId);
+        return ResponseEntity.ok(isDuplicate);
+    }
+
+    // 회원가입시 닉네임 중복체크
+    @PostMapping("/checkNickname")
+    public ResponseEntity<Boolean> checkNickname(@RequestBody Map<String, String> requestBody) {
+        String nickname = requestBody.get("nickname");
+        boolean isDuplicate = usersService.isLoginCheckNickname(nickname);
+        return ResponseEntity.ok(isDuplicate);
+    }
+
+    // 회원가입시 email 중복체크
+    @PostMapping("/checkEmail")
+    public ResponseEntity<Boolean> checkEmail(@RequestBody Map<String, String> requestBody) {
+        String email = requestBody.get("email");
+        boolean isDuplicate = usersService.isLoginCheckEmail(email);
+        return ResponseEntity.ok(isDuplicate);
+    }
+//
+//    // 회원탈퇴
+//    @PostMapping("/withdrawal")
+//    public ResponseEntity<Void> doWithdrawal(@RequestBody Map<String, String> request) {
+//        String password = request.get("password");
+//
+//        Users user = usersService.findUserById(SecurityUtil.getLoggedInUser().getId());
+//        if(usersService.softDelete(user, password)) {
+//            return ResponseEntity.ok().build();
+//        }
+//
+//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+//    }
 }
