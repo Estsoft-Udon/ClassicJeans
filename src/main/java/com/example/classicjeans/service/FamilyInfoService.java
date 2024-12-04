4163af8d74e774a0a72462e00765f2bc95af773c
Sang Won Lee <freerusya@naver.com>
2024-12-04 10:53:08 +0900

    feat: 가족 정보 저장 기능 추가

--- src/main/java/com/example/classicjeans/service/FamilyInfoService.java
+++ src/main/java/com/example/classicjeans/service/FamilyInfoService.java
@@ -1,18 +1,28 @@
 package com.example.classicjeans.service;
 
+import com.example.classicjeans.dto.request.FamilyInfoRequest;
+import com.example.classicjeans.dto.response.FamilyInfoResponse;
+import com.example.classicjeans.entity.FamilyInfo;
+import com.example.classicjeans.entity.Users;
 import com.example.classicjeans.repository.FamilyInfoRepository;
-import com.example.classicjeans.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FamilyInfoService {
 
-    private final UsersRepository usersRepository;
+    private final UsersService usersService;
     private final FamilyInfoRepository familyInfoRepository;
 
-    public void saveFamily() {
+    // 가족 정보 저장
+    public FamilyInfoResponse saveFamily(Long userId, FamilyInfoRequest request) {
+        // 추후에 로그인 중인 유저 정보로 변경 필요
+        Users user = usersService.findUserById(8L);
 
+        FamilyInfo familyInfo = familyInfoRepository.save(new FamilyInfo(user, request.getName(), request.getGender(), request.getDateOfBirth(), request.getRelationship()));
+        return FamilyInfoResponse.convertFamilyInfo(familyInfo);
     }
 }
