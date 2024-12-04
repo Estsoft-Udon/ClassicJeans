4ffb4ef0d9ac53ab52fdcaf52167f3b67192ff9c
Sang Won Lee <freerusya@naver.com>
2024-12-04 10:54:37 +0900

    feat: 가족정보 저장api 추가

--- src/main/java/com/example/classicjeans/controller/FamilyInfoController.java
+++ src/main/java/com/example/classicjeans/controller/FamilyInfoController.java
@@ -1,5 +1,7 @@
 package com.example.classicjeans.controller;
 
+import com.example.classicjeans.dto.request.FamilyInfoRequest;
+import com.example.classicjeans.dto.response.FamilyInfoResponse;
 import com.example.classicjeans.service.FamilyInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -11,4 +13,9 @@ import org.springframework.web.bind.annotation.*;
 public class FamilyInfoController {
     private final FamilyInfoService familyInfoService;
 
+    // 가족 정보 저장 API
+    @PostMapping("/{userId}")
+    public FamilyInfoResponse saveFamily(@PathVariable Long userId, @RequestBody FamilyInfoRequest request) {
+        return familyInfoService.saveFamily(userId, request);
+    }
 }
