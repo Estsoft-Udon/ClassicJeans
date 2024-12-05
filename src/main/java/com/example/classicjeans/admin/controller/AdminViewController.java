a5258666a82ca43bc647a91ad5d60497382fdfce
dejeong <ahjeong921@gmail.com>
2024-12-05 16:53:03 +0900

    fix : 작업 전 내용으로 되돌림

--- src/main/java/com/example/classicjeans/admin/controller/AdminViewController.java
+++ src/main/java/com/example/classicjeans/admin/controller/AdminViewController.java
@@ -2,27 +2,11 @@ package com.example.classicjeans.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
 public class AdminViewController {
-
-    // 관리자 메인
-    @GetMapping
+    @GetMapping("/admin")
     public String adminMain() {
         return "admin/admin_index";
-    }
-
-    // 예약 목록
-    @GetMapping("/reservation_list")
-    public String reservationList() {
-        return "admin/reservation/reservation_list";
-    }
-
-    // 예약 상세
-    @GetMapping("/reservation_edit")
-    public String reservationEdit() {
-        return "admin/reservation/reservation_edit";
     }
 }
