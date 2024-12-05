3f583c5f30a5ceca9cd88a57a9681695a6cf54a9
Sang Won Lee <freerusya@naver.com>
2024-12-05 10:27:03 +0900

    feat: 선택한 검사 대상 세션에 저장

--- src/main/java/com/example/classicjeans/controller/CheckupController.java
+++ src/main/java/com/example/classicjeans/controller/CheckupController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.List;
 
@@ -17,6 +18,7 @@ import static com.example.classicjeans.util.SecurityUtil.getLoggedInUser;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/checkout")
+@SessionAttributes("selectedUser")
 public class CheckupController {
     private final UsersService usersService;
     private final FamilyInfoService familyInfoService;
