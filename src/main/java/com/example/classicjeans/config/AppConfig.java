4e65b3047e4639b8cbbb0318e4cc1fc9de741b90
Sang Won Lee <freerusya@naver.com>
2024-12-06 11:42:46 +0900

    feat: ObjectMapper 설정에 JavaTimeModule 등록 및 날짜 형식 ISO로 변경 추가

--- src/main/java/com/example/classicjeans/config/AppConfig.java
+++ src/main/java/com/example/classicjeans/config/AppConfig.java
@@ -2,6 +2,8 @@ package com.example.classicjeans.config;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,9 +16,19 @@ public class AppConfig {
         return new RestTemplate();
     }
 
-    // SSE-Streaming Json parse 추가
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper().configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Java 8 시간 API 모듈 등록
+        objectMapper.registerModule(new JavaTimeModule());
+
+        // 타임스탬프 대신 ISO 형식 사용
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+        // Single quote 허용
+        objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+
+        return objectMapper;
     }
 }
