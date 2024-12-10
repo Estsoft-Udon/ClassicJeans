9f4b52ecf61d8da4f237be4092d5a498d7d4ba54
Sang Won Lee <freerusya@naver.com>
2024-12-10 11:19:24 +0900

    feat: 흡연 상태에 건강 지수 계산을 위한 impactScore 추가

--- src/main/java/com/example/classicjeans/enums/questionnaire/SmokingStatus.java
+++ src/main/java/com/example/classicjeans/enums/questionnaire/SmokingStatus.java
@@ -1,18 +1,16 @@
 package com.example.classicjeans.enums.questionnaire;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 // 흡연 상태
+@Getter
+@AllArgsConstructor
 public enum SmokingStatus {
-    CURRENTLY_SMOKING("현재 흡연 중"),
-    FORMER_SMOKER("과거에 흡연했으나 현재는 금연"),
-    NON_SMOKER("비흡연자");
+    CURRENTLY_SMOKING("현재 흡연 중", 5),
+    FORMER_SMOKER("과거에 흡연했으나 현재는 금연", 2),
+    NON_SMOKER("비흡연자", 0);
 
     private final String displayName;
-
-    SmokingStatus(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
+    private final int impactScore;
 }
