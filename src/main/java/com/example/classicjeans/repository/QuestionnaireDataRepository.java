c66ebf99196c97a1f7d0bf3916346b6dbe55650f
Sang Won Lee <freerusya@naver.com>
2024-12-10 16:19:33 +0900

    feat: 기본검사 결과목록 조회 페이지로 변경

--- src/main/java/com/example/classicjeans/repository/QuestionnaireDataRepository.java
+++ src/main/java/com/example/classicjeans/repository/QuestionnaireDataRepository.java
@@ -3,16 +3,17 @@ package com.example.classicjeans.repository;
 import com.example.classicjeans.entity.FamilyInfo;
 import com.example.classicjeans.entity.QuestionnaireData;
 import com.example.classicjeans.entity.Users;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
-import java.util.List;
 
 @Repository
 public interface QuestionnaireDataRepository extends JpaRepository<QuestionnaireData, Long> {
 
     @Query("SELECT q FROM QuestionnaireData q WHERE (q.userId = :user AND q.familyId IS NULL) OR (q.familyId IN :familyInfos AND q.userId IS NULL)")
-    List<QuestionnaireData> findByUserOrFamily(Users user, Collection<FamilyInfo> familyInfos);
+    Page<QuestionnaireData> findByUserOrFamily(Users user, Collection<FamilyInfo> familyInfos, Pageable pageable);
 }
