7c73c8993d03be6ac83f494c89f0fe1709583bc6
jingeon <ephigh1397@naver.com>
2024-12-10 18:36:15 +0900

    refactor: API 명세서에 맞게 매핑 주소 변경 / 사용하지 않는 api 삭제

--- src/main/java/com/example/classicjeans/controller/rest/ReservationController.java
+++ src/main/java/com/example/classicjeans/controller/rest/ReservationController.java
@@ -6,14 +6,11 @@ import com.example.classicjeans.entity.Users;
 import com.example.classicjeans.service.ReservationNotificationService;
 import com.example.classicjeans.service.ReservationService;
 import com.example.classicjeans.entity.Reservation;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.example.classicjeans.util.SecurityUtil.getLoggedInUser;
@@ -24,7 +21,6 @@ import static com.example.classicjeans.util.SecurityUtil.getLoggedInUser;
 public class ReservationController {
     private final ReservationService reservationService;
     private final ReservationNotificationService notificationService;
-    private final ObjectMapper objectMapper;
 
     @GetMapping("/reservation/stream")
     public ResponseEntity<SseEmitter> connect() {
@@ -46,31 +42,6 @@ public class ReservationController {
         }
 
         return ResponseEntity.ok(reservationService.addReservation(request));
-    }
-
-    @GetMapping("/reservation")
-    public void sendNotification() throws JsonProcessingException {
-        LocalDateTime oneDayAfter = LocalDateTime.now().plusDays(1);
-
-        boolean processing = true;
-        while (processing) {
-            Reservation reservation = reservationService.getNextReservation();
-            if (reservation != null) {
-                // 예약시간이 현재 시간의 하루전보다 이전이면 알림 후 알림 여부 true로
-                if (reservation.getTime().isBefore(oneDayAfter)) {
-                    // 여기서 알림 전송
-                    String message = objectMapper.writeValueAsString(reservation);
-
-                    notificationService.sendNotification(reservation.getUser().getId(), message);
-                    reservationService.notifyReservation(reservation);
-                } else {
-                    reservationService.addReservationToQueue(reservation);
-                    processing = false; // 종료 조건 설정
-                }
-            } else {
-                processing = false; // null일 때 종료
-            }
-        }
     }
 
     @GetMapping("/reservations")
@@ -99,15 +70,10 @@ public class ReservationController {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/reservation/read/{id}")
-    public ResponseEntity<Reservation> setReservationRead(@PathVariable Long id) {
-
-        return ResponseEntity.ok(reservationService.setReadTrue(id));
-    }
-
-    @DeleteMapping("/reservation/read/{id}")
-    public ResponseEntity<Reservation> setReservationUnRead(@PathVariable Long id) {
-
-        return ResponseEntity.ok(reservationService.setReadFalse(id));
+    @PatchMapping("/reservation/read/{id}")
+    public ResponseEntity<Reservation> toggleReservationReadStatus(@PathVariable Long id) {
+        // 예약의 현재 상태를 가져와서 토글
+        Reservation reservation = reservationService.toggleReadStatus(id);
+        return ResponseEntity.ok(reservation);
     }
 }
