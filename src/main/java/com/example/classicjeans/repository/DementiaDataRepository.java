de4c27b3338fa2dde671635904281627a947d1dd
Sang Won Lee <freerusya@naver.com>
2024-12-10 16:05:31 +0900

    feat: 치매검사 결과목록 조회를 위한 쿼리 작성

--- src/main/java/com/example/classicjeans/repository/DementiaDataRepository.java
+++ src/main/java/com/example/classicjeans/repository/DementiaDataRepository.java
@@ -1,9 +1,18 @@
 package com.example.classicjeans.repository;
 
 import com.example.classicjeans.entity.DementiaData;
+import com.example.classicjeans.entity.FamilyInfo;
+import com.example.classicjeans.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface DementiaDataRepository extends JpaRepository<DementiaData, Long> {
+
+    @Query("SELECT d FROM DementiaData d WHERE (d.userId = :user AND d.familyId IS NULL) OR (d.familyId IN :familyInfos AND d.userId IS NULL)")
+    List<DementiaData> findByUserOrFamily(Users user, Collection<FamilyInfo> familyInfos);
 }
