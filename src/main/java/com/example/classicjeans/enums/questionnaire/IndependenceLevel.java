75977b135e7cfb811705c2af8ad451fddd04e350
Sang Won Lee <freerusya@naver.com>
2024-12-10 11:24:31 +0900

    feat: 독립성 수준에 건강 지수 계산을 위한 impactScore 추가

--- src/main/java/com/example/classicjeans/enums/questionnaire/IndependenceLevel.java
+++ src/main/java/com/example/classicjeans/enums/questionnaire/IndependenceLevel.java
@@ -1,18 +1,16 @@
 package com.example.classicjeans.enums.questionnaire;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 // 독립성 수준
+@Getter
+@AllArgsConstructor
 public enum IndependenceLevel {
-    FULLY_INDEPENDENT("완전히 독립적"),
-    PARTIALLY_DEPENDENT("부분적으로 의존적"),
-    FULLY_DEPENDENT("완전히 의존적");
+    FULLY_INDEPENDENT("완전히 독립적", 5),
+    PARTIALLY_DEPENDENT("부분적으로 의존적", -3),
+    FULLY_DEPENDENT("완전히 의존적", -7);
 
     private final String displayName;
-
-    IndependenceLevel(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
+    private final int impactScore;
 }
