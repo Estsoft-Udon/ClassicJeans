0889f588c2cdf4e45964bd5182d1571da7854143
jingeon <ephigh1397@naver.com>
2024-12-11 16:08:35 +0900

    refactor: 로그인되지 않았을 때 이벤트 리스너 추가 X

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -28,10 +28,12 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     // 종 클릭 시 알림 토글
-    bellIcon.addEventListener('click', () => {
-        notificationList.classList.toggle('visible');
-        notificationList.classList.toggle('hidden');
-    });
+    if (window.getComputedStyle(bellIcon).display !== 'none') {
+        bellIcon.addEventListener('click', () => {
+            notificationList.classList.toggle('visible');
+            notificationList.classList.toggle('hidden');
+        });
+    }
 
     // 외부 클릭 시 알림창 닫기
     document.addEventListener('click', (event) => {
