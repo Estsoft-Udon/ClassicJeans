c7857fd6ffb5cd6faa0d797ea15c70f09b359686
Sang Won Lee <freerusya@naver.com>
2024-12-11 09:16:59 +0900

    feat: 치매 검사 전체 목록 조회 List로 변경

--- src/main/java/com/example/classicjeans/repository/DementiaDataRepository.java
+++ src/main/java/com/example/classicjeans/repository/DementiaDataRepository.java
@@ -10,13 +10,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface DementiaDataRepository extends JpaRepository<DementiaData, Long> {
 
     // 기본 전체 목록 조회
     @Query("SELECT d FROM DementiaData d WHERE (d.userId = :user AND d.familyId IS NULL) OR (d.familyId IN :familyInfos AND d.userId IS NULL)")
-    Page<DementiaData> findByUserOrFamily(Users user, Collection<FamilyInfo> familyInfos, Pageable pageable);
+    List<DementiaData> findByUserOrFamily(Users user, Collection<FamilyInfo> familyInfos);
 
     // 본인 목록 조회
     Page<DementiaData> findByUserId(Users user, Pageable pageable);
