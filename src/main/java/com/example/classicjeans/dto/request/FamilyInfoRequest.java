332d2728401939766239eba83b89aef3aec0933a
goldsonge <ty7968@gmail.com>
2024-12-11 15:44:42 +0900

    fix: 마이페이지 가족 정보 추가 오류 수정 및 링크 수정

--- src/main/java/com/example/classicjeans/dto/request/FamilyInfoRequest.java
+++ src/main/java/com/example/classicjeans/dto/request/FamilyInfoRequest.java
@@ -3,11 +3,13 @@ package com.example.classicjeans.dto.request;
 import com.example.classicjeans.enums.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class FamilyInfoRequest {
     private String name;
     private Gender gender;
