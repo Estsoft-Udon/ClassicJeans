b2bdd7a053993b3b21e78a3d07396864f770f65d
Sang Won Lee <freerusya@naver.com>
2024-12-11 17:58:41 +0900

    feat: 프롬프트 변경으로 파싱에 사용되는 정규식 변경

--- src/main/java/com/example/classicjeans/util/RegexPatterns.java
+++ src/main/java/com/example/classicjeans/util/RegexPatterns.java
@@ -5,11 +5,11 @@ public class RegexPatterns {
     public static final String IMPROVEMENT_SUGGESTIONS_PATTERN = "(?i)### 개선 방법 \\(improvement\\s*suggestions\\)[\\s\\S]*?(\\d+\\.\\s.*?)(?=\\n$|$)";
 
     public static final String AGE_GROUP_PATTERN = "연령대:\\s*(\\d+대)";
-    public static final String AVERAGE_HEIGHT_PATTERN = "한국인 (남성|여성) 평균: 약 (\\d+(\\.\\d+)?) cm";
-    public static final String AVERAGE_WEIGHT_PATTERN = "한국인 (남성|여성) 평균: 약 (\\d+(\\.\\d+)?) kg";
-    public static final String SMOKING_RATE_PATTERN = "한국인 (남성|여성) 흡연율: 약 (\\d+(\\.\\d+)?)%";
-    public static final String DRINKING_RATE_PATTERN = "한국인 (남성|여성) 음주율: 약 (\\d+(\\.\\d+)?)%";
-    public static final String EXERCISE_RATE_PATTERN = "한국인 (남성|여성) 규칙적 운동 비율: 약 (\\d+(\\.\\d+)?)%";
+    public static final String AVERAGE_HEIGHT_PATTERN = "\\((\\d+대) (남성|여성) 평균: 약 (\\d+(\\.\\d+)?)\\s*cm";
+    public static final String AVERAGE_WEIGHT_PATTERN = "\\((\\d+대) (남성|여성) 평균: 약 (\\d+(\\.\\d+)?)\\s*kg";
+    public static final String SMOKING_RATE_PATTERN = "한국인 (남성|여성) 흡연율: 약 (\\d+(\\.\\d+)?)\\s*%";
+    public static final String DRINKING_RATE_PATTERN = "한국인 (남성|여성) 음주율: 약 (\\d+(\\.\\d+)?)\\s*%";
+    public static final String EXERCISE_RATE_PATTERN = "한국인 (남성|여성) 걷기 실천율: 약 (\\d+(\\.\\d+)?)\\s*%";
 
     public static final String URL_PATTERN = "\\[\\(출처\\d+\\)]\\(https?://[\\w./?&=-]+\\)";
 }
