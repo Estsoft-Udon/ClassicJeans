677dcd610ba9091f8f4ff2133225a9e284b7e3a0
jingeon <ephigh1397@naver.com>
2024-12-12 20:18:18 +0900

    refactor: redirect:/ 로 변경

--- src/main/java/com/example/classicjeans/controller/view/UserViewController.java
+++ src/main/java/com/example/classicjeans/controller/view/UserViewController.java
@@ -64,9 +64,8 @@ public class UserViewController {
             usersService.register(request);
 
             session.removeAttribute("uniqueKey");
-            session.removeAttribute("providerId");
 
-            return "redirect:success";
+            return "redirect:/success";
         } catch (Exception e) {
             model.addAttribute("error", e.getMessage());
             return "member/signup";
@@ -127,7 +126,7 @@ public class UserViewController {
             return "member/change-pw";
         } else {
             model.addAttribute("errorMessage", "현재 비밀번호가 일치하지 않습니다.");
-            return "redirect:member/change-pw";
+            return "redirect:/member/change-pw";
         }
     }
 
@@ -177,7 +176,7 @@ public class UserViewController {
     public String editProfile(@ModelAttribute UsersRequest request) {
         Users user = usersService.findUserById(getLoggedInUser().getId());
         usersService.update(user.getId(), request);
-        return "redirect:member/mypage";
+        return "redirect:/member/mypage";
     }
 
     // 가족 정보 수정
