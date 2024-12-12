eec4291add2fc72fc05bcb1d4d6fb03eb01f590c
SongJwans <yhh5916@naver.com>
2024-12-12 17:06:49 +0900

    refactor : build 추가 및 TestController 삭제

--- src/main/java/com/example/classicjeans/controller/view/MainController.java
+++ src/main/java/com/example/classicjeans/controller/view/MainController.java
@@ -1,16 +1,14 @@
 package com.example.classicjeans.controller.view;
 
-import com.example.classicjeans.entity.Users;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import static com.example.classicjeans.util.SecurityUtil.*;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
-    @RequestMapping("/")
+
+    @GetMapping("/")
     public String main() {
         return "index";
     }
