d42bb4600911c85d79f9d106c7b939621327ece3
jingeon <ephigh1397@naver.com>
2024-12-17 09:05:59 +0900

    feat: Add Swagger configuration for API documentation

--- src/main/java/com/example/classicjeans/controller/rest/FamilyInfoController.java
+++ src/main/java/com/example/classicjeans/controller/rest/FamilyInfoController.java
@@ -3,6 +3,7 @@ package com.example.classicjeans.controller.rest;
 import com.example.classicjeans.dto.request.FamilyInfoRequest;
 import com.example.classicjeans.dto.response.FamilyInfoResponse;
 import com.example.classicjeans.service.FamilyInfoService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -17,18 +18,21 @@ public class FamilyInfoController {
     private final FamilyInfoService familyInfoService;
 
     // 가족 정보 저장 API
+    @Operation(summary = "가족 정보 저장")
     @PostMapping
     public List<FamilyInfoResponse> saveFamily(@RequestBody List<FamilyInfoRequest> request) {
         return familyInfoService.saveFamily(null, request);
     }
 
     // 가족 정보 조회 API
+    @Operation(summary = "가족 정보 조회")
     @GetMapping
     public List<FamilyInfoResponse> getFamilyByUserId(Long userId) {
         return familyInfoService.findFamilyByUserId(userId);
     }
 
     // 가족 정보 삭제 API
+    @Operation(summary = "가족 정보 삭제")
     @DeleteMapping("/{familyId}")
     public void deleteFamily(@PathVariable Long familyId) {
         familyInfoService.deleteFamily(familyId);
